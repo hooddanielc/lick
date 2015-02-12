37a9d016f456cd0dad452146860abdd09cd7802d
Daniel Hood <hood.danielc@gmail.com>
2015-02-11 18:57:51 -0800

    Adds more example code to simple.cc

--- example/simple.cc
+++ example/simple.cc
@@ -31,9 +31,46 @@ public:
   }
 };
 
-FIXTURE(initialization) {
+FIXTURE(is_equal) {
   AddNumbers a;
-  EXPECT_EQ(a.getA(), 1) << "it does not initialize with 0";
+  EXPECT_EQ(a.getA(), 0) << "it does not initialize with 0";
+}
+
+FIXTURE(not_equal) {
+  AddNumbers a;
+  EXPECT_NE(a.getA(), 1) << "0 != 1";
+}
+
+FIXTURE(falsy_values) {
+  AddNumbers a;
+  EXPECT_FALSE(a.getSum() < 0) << "0 + 0 is not negative";
+}
+
+FIXTURE(true_values) {
+  AddNumbers a;
+  EXPECT_TRUE(a.getSum() == 0) << "0 + 0 == 0";
+}
+
+FIXTURE(greater_than) {
+  AddNumbers a;
+  EXPECT_GT(a.getA(), -1) << "0 > -1";
+}
+
+FIXTURE(lesser_than) {
+  AddNumbers a;
+  EXPECT_LT(a.getA(), 1) << "0 < 1";
+}
+
+FIXTURE(greater_than_or_equal) {
+  AddNumbers a;
+  EXPECT_GE(a.getA(), 0) << "0 >= 0";
+  EXPECT_GE(a.getA(), -1) << "0 >= -1";
+}
+
+FIXTURE(lesser_than_or_equal) {
+  AddNumbers a;
+  EXPECT_LE(a.getA(), 0) << "0 <= 0";
+  EXPECT_LE(a.getA(), 1) << "0 <= -1";
 }
 
 int main(int argc, char *argv[]) {
